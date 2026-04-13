b4b8046b5a9c8a4587151763e8d71c7178289f4c
Maik Lorenz <maik.lorenz@pm.me>
2026-04-13 11:03:46 +0200

    fix: remove unit/ folder (artifact, moved to tests/unit)

--- unit/mocks/mqtt_mock.h
+++
@@ -1,7 +0,0 @@
-#include "mqtt_iface.h"
-
-const mqtt_iface_t *mqtt_mock_get_iface(void);
-void mqtt_mock_reset(void);
-bool mqtt_mock_is_initialized(void);
-bool mqtt_mock_is_connected(void);
-float mqtt_mock_last_published_temp(void);
